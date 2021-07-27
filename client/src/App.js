3a6f80a48d19c30b56be7772962ed0476e048cf3
cljuiza <christian.juiza@gmail.com>
2021-07-27 12:11:41 -0500

    update component Searcher

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+//Components
+import NavBar from "./Components/NavBar/NavBar";
+import GistsDetail from "./Components/GitsDetail/GistsDetail";
+import Searcher from "./Components/Searcher/Searcher";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <h1>El Tiempo</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/" component={Searcher}></Route>
+        <Route path="/gists/:id" component={GistsDetail}></Route>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Components/Searcher/Searcher.jsx
+++ client/src/Components/Searcher/Searcher.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+
+//components
+import Gists from "../Gists/Gists";
 
 const Searcher = () => {
-    return (
-        <div>
-            <h2>Searcher</h2>
-        </div>
-    );
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(search!==""){
+      // action get 
+    }
+    else alert("Digite una receta")
+  };
+
+  return (
+    <div>
+      <h2>Searcher</h2>
+      <form onSubmit={handleSubmit}> 
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="search..."
+          value={search}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Search" /> 
+      </form>
+      <Gists stateSearch={search}/>
+    </div>
+  );
 };
 
 export default Searcher;
